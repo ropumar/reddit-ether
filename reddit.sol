663e884ae2d016148492741b81ac65fcca538c32
ropumar <rodrigopumar@gmail.com>
2019-10-21 21:47:25 -0300

    before promise

--- reddit.sol
+++ reddit.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.5.12;
-pragma experimental ABIEncoderV2;
 
 contract Reddit {
     uint public maxLength;
@@ -8,7 +7,8 @@ contract Reddit {
         string contents;
         address senderAddress;
         uint timestamp;
-  }
+        uint votes;
+    }
     _postContent[] Messages; 
     
     constructor() public {
@@ -16,14 +16,11 @@ contract Reddit {
         postMessage("Hello World");
     }
   
-    function getMessages()public view returns ( _postContent[] memory){
-    return Messages;
-  }
 
-  function postMessage(string memory _message) public{
-    require(bytes(_message).length <= maxLength, "Message too long");
-    Messages.push(_postContent(_message, msg.sender, block.timestamp));
-  }
+    function postMessage(string memory _message) public{
+        require(bytes(_message).length <= maxLength, "Message too long");
+        Messages.push(_postContent(_message, msg.sender, block.timestamp, 0));
+    }
     
 
     function setMaxLength(uint _maxLength) public {
@@ -32,25 +29,36 @@ contract Reddit {
     
     modifier checkValidIndex(uint i) {
         require(i < getMessageCount());
-        require(i >= 0); _;
-  }
-  function getMessageCount()
-    public view returns (uint) {
-    return Messages.length;
-  }
+        require(i >= 0); 
+        _;
+    }
+    function getMessageCount()
+        public view returns (uint) {
+        return Messages.length;
+    }
 
-  function getMessageContents(uint i) checkValidIndex(i)
-    public view returns (string memory) {
-    return Messages[i].contents;
-  }
+    function getMessageContents(uint i) checkValidIndex(i)
+        public view returns (string memory) {
+        return Messages[i].contents;
+    }
 
-  function getMessageAddress(uint i) checkValidIndex(i)
-    public view returns (address) {
-    return Messages[i].senderAddress;
-  }
+    function getMessageAddress(uint i) checkValidIndex(i)
+        public view returns (address) {
+        return Messages[i].senderAddress;
+    }
 
-  function getMessageTimestamp(uint i) checkValidIndex(i)
-    public view returns (uint) {
-    return Messages[i].timestamp;
-}
+    function getMessageTimestamp(uint i) checkValidIndex(i)
+        public view returns (uint) {
+        return Messages[i].timestamp;
+    }
+
+    function getVotes(uint i) checkValidIndex(i)
+        public view returns (uint) {
+        return Messages[i].votes;
+    }
+
+    function payMessager(uint index) public payable returns (uint) {
+        Messages[index].votes = Messages[index].votes + 1;
+        Messages[index].senderAddress.transfer(1);
+    }
 }
